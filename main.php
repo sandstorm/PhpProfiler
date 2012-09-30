2e92ce8802a0094da3aae01b55cfe3041fe805f5
Sebastian Kurfuerst <sebastian@typo3.org>
2012-09-30 07:59:32 +0200

    [!!!][TASK] composerify PhpProfiler
    This commit contains the result of applying migration
     TYPO3.FLOW3-201209201112.
    to this package.
    
    Migration: TYPO3.FLOW3-201209201112

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 
-require('Classes/Profiler.php');
-require('Classes/Domain/Model/EmptyProfilingRun.php');
-require('Classes/Domain/Model/ProfilingRun.php');
+require('Classes/SandstormMedia/PhpProfiler/Profiler.php');
+require('Classes/SandstormMedia/PhpProfiler/Domain/Model/EmptyProfilingRun.php');
+require('Classes/SandstormMedia/PhpProfiler/Domain/Model/ProfilingRun.php');
 
 ?>
